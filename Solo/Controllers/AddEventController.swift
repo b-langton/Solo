5ecba7a65d997898e246fcbc14a585242f328bd2
Ben Langton <blangton@g.hmc.edu>
2019-08-20 14:16:12 -0500

    add event ui

--- Solo/Controllers/AddEventController.swift
+++ Solo/Controllers/AddEventController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AddEventController.swift
 //  Solo
 //
 //  Created by user157824 on 8/18/19.
@@ -10,10 +10,42 @@ import UIKit
 
 class AddEventController: UIViewController {
 
+
+    
+    @IBOutlet weak var endDateInputText: UITextField!
+    @IBOutlet weak var dateInputText: UITextField!
+    @IBOutlet weak var Address: UITextField!
+    @IBOutlet weak var EventName: UITextField!
+    @IBOutlet weak var Desc: UITextField!
+    
+    private var datePicker: UIDatePicker!
+    private var datePicker2: UIDatePicker!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        datePicker = UIDatePicker()
+        datePicker.datePickerMode = .dateAndTime
+        datePicker.addTarget(self, action: #selector(AddEventController.dateChanged(datePicker:)),for: .valueChanged)
+        dateInputText.inputView = datePicker
+        
+        datePicker2 = UIDatePicker()
+        datePicker2.datePickerMode = .dateAndTime
+        datePicker2.addTarget(self, action: #selector(AddEventController.dateChanged2(datePicker:)), for: .valueChanged)
+        endDateInputText.inputView = datePicker2
+    }
+    
+    @objc func dateChanged(datePicker: UIDatePicker){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd hh:mm"
+        dateInputText.text = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
+    @objc func dateChanged2(datePicker: UIDatePicker){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd hh:mm"
+        endDateInputText.text = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }        // Do any additional setup after loading the view.
     }
     
 
@@ -27,4 +59,4 @@ class AddEventController: UIViewController {
     }
     */
 
-}
+
